c1ca1d3e8b9060be0feb96f744e7b2f7e209ce3e
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-04 14:14:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def key_for_min_value(name_hash)
-  minimum_value = 0
-  key = ''
+  minimum_value = ""
+  key = ""
   name_hash.each do |name, value|
+    binding.pry
      if value < minimum_value
        minimum_value = value
        key = name 
