5c086d27346301e63158d615a90964ec2a3eed30
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-04 14:41:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,13 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  starting_value = 0
   minimum_value = nil
   minimum_item = nil
   if name_hash == {}
     return nil 
   else 
     name_hash.each do |item, value|
-      if value < starting_value
+      if value < minimum_value
         minimum_value = value
         minimum_item = item
       end
